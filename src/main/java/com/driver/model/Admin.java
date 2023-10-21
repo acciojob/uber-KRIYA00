de9816911e503d1dbb4f1caf0b4ce06e44fc81ac
ysi <yshouldi@gmail.com>
2023-10-21 23:33:03 +0530

    willitwork

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,47 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Admin")
+
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer adminId;
+    private String userName;
+    private String password;
+
+    public Admin() {
+    }
+
+    public Admin(Integer adminId, String userName, String password) {
+        this.adminId = adminId;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUsername(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
